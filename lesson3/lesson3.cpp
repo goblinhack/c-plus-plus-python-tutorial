71b619c81571027f69162b05c6a2ce5e831f4f29
Neil McGill <goblinhack@gmail.com>
2019-10-06 13:42:18 -0400

    better screenshot

--- lesson3/lesson3.cpp
+++ lesson3/lesson3.cpp
@@ -26,10 +26,22 @@ int main (int argc, char *argv[])
 
     term.clear_screen();
     std::cout << term.get_code(term.FOREGROUND_RED);
-    Line l(&term, Point(3, 10), Point(20,31));
-    l.draw('x');
+    Line(&term, Point(4, 4), Point(term.size.x - 4, term.size.y - 4)).draw('r');
+    std::cout << term.get_code(term.FOREGROUND_GREEN);
+    Line(&term, Point(term.size.x - 4, 4),
+         Point(term.size.x - 20, term.size.y - 4)).draw('r');
+
+    std::cout << term.get_bgfg_code(term.BACKGROUND_BLUE,
+                                        term.FOREGROUND_WHITE);
+    Line(&term, Point(0, 0), Point(0, term.size.y - 2)).draw(' ');
+    Line(&term, Point(term.size.x - 1, 0),
+         Point(term.size.x - 1, term.size.y - 2)).draw(' ');
+    Line(&term, Point(0, 0), Point(term.size.x - 1, 0)).draw(' ');
+    Line(&term, Point(0, term.size.y - 2),
+         Point(term.size.x - 1, term.size.y - 2)).draw(' ');
 
     term.set_cursor_bottom_left();
+    std::cout << term.get_code(term.RESET);
     std::cout << term.get_code(term.FOREGROUND_GREEN);
     std::cout << "C++: Line draw demo";
     std::cout << term.get_code(term.RESET);
--- lesson3/lesson3.py
+++ lesson3/lesson3.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 '''
 lesson3 - get the terminal size
 '''
@@ -15,10 +16,22 @@ def lesson3():
 
     term.clear_screen();
     sys.stdout.write(term.get_code(term.Code.FOREGROUND_RED))
-    l = Line(term, Point(3, 10), Point(20,31))
-    l.draw('x');
+    Line(term, Point(4, 4), Point(term.size.x - 4, term.size.y - 4)).draw('r')
+    sys.stdout.write(term.get_code(term.Code.FOREGROUND_GREEN))
+    Line(term, Point(term.size.x - 4, 4),\
+         Point(term.size.x - 20, term.size.y - 4)).draw('r')
+
+    sys.stdout.write(term.get_bgfg_code(term.Code.BACKGROUND_BLUE,
+                                        term.Code.FOREGROUND_WHITE))
+    Line(term, Point(0, 0), Point(0, term.size.y - 2)).draw(' ')
+    Line(term, Point(term.size.x - 1, 0), \
+         Point(term.size.x - 1, term.size.y - 2)).draw(' ')
+    Line(term, Point(0, 0), Point(term.size.x - 1, 0)).draw(' ')
+    Line(term, Point(0, term.size.y - 2), \
+         Point(term.size.x - 1, term.size.y - 2)).draw(' ')
 
     term.set_cursor_bottom_left()
+    sys.stdout.write(term.get_code(term.Code.RESET))
     sys.stdout.write(term.get_code(term.Code.FOREGROUND_GREEN))
     print("Python: Line draw demo")
     sys.stdout.write(term.get_code(term.Code.RESET))
